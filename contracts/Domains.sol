e7a6c34c69ff0bf1d4dc7d2feb92797022178f5c
Jatin Mehta <jatinmehta.jm@gmail.com>
2022-06-01 21:07:11 -0400

    Set up domain/resolver

--- contracts/Domains.sol
+++ contracts/Domains.sol
@@ -1,10 +1,40 @@
+//SPDX-License-Identifier: Unlicensed
 
 pragma solidity^0.8.10;
 
 import "hardhat/console.sol";
 
 contract Domains {
+    mapping(string => address) public domains;
+
+    mapping(string => string) public records;
+
     constructor() {
         console.log("Init domains contract");
     }
+
+    function register(string calldata name) public {
+        //Here we’re checking that the address of the domain you’re trying to register
+        //is the same as the zero address. The zero address in Solidity is sort of like the void (in the literal
+        //sense) where everything comes from. When an address mapping is initialized, all entries in it point to the
+        //zero address. So if a domain hasn’t been registered, it’ll point to the zero address!
+        require(domains[name] == address(0));
+
+        domains[name] = msg.sender;
+        console.log("%s has registered a domain", msg.sender);
+    }
+
+    function getAddress(string calldata name) public view returns (address) {
+        return domains[name];
+    }
+
+    function setRecord(string calldata name, string calldata record) public {
+        require(domains[name] == msg.sender);
+        records[name] = record;
+    }
+
+    function getRecord(string calldata name) public view returns(string memory) {
+        return records[name];
+    }
+
 }
---
+++ scripts/run.js
@@ -0,0 +1,44 @@
+
+const main = async () => {
+    //hre: Hardhat Runtime Environment, ie, the object containing all functionality of Hardhat.
+    //So what does this mean? Every time you run a terminal command that starts with npx hardhat
+    // you are getting this hre object built on the fly using the hardhat.config.js specified in your code!
+    // This means you will never have to actually do some sort of import into your files
+    const [owner, randomAccount] = await hre.ethers.getSigners();
+
+    //Compiles our solidity contract
+    const domainContractFactory = await hre.ethers.getContractFactory('Domains');
+
+    //Deploys it to the chain configured in hardhat.config.js
+    const domainContract = await domainContractFactory.deploy();
+
+    //Wait till the contract is mined and deployed to the local blockchain especially created for this account
+    //which will be destroyed once this script completes
+    await domainContract.deployed();
+    console.log("Contract deployed to: " + domainContract.address);
+    console.log("Contract deployed by:" + owner.address);
+    console.log("What the hell is account: " + randomAccount);
+
+    let txn = await domainContract.register("0xshinigami");
+    await txn.wait();
+
+    const domainAddress = await domainContract.getAddress("0xshinigami");
+    console.log("Address for domain name 0xshinigami: " + domainAddress);
+    
+    // txn = await domainContract.connect(randomAccount).setRecord("0xshinigami", "Setting domain w/o being the
+    // sender, should revert.");
+    // await txn.wait();
+
+};
+
+const runMain = async() => {
+    try {
+        await main();
+        process.exit(0);
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+};
+
+runMain();
