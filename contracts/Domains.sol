690281f94de8bbc0ddbc0e28b27ee65839d98098
Jatin Mehta <jatinmehta.jm@gmail.com>
2022-06-02 00:59:41 -0400

    Adding payable contract + stringUtils

--- contracts/Domains.sol
+++ contracts/Domains.sol
@@ -3,17 +3,47 @@
 pragma solidity^0.8.10;
 
 import "hardhat/console.sol";
+import {StringUtils} from "../libraries/StringUtils.sol";
 
 contract Domains {
+
+    //Top level domain like .eth
+    string public tld;
+
     mapping(string => address) public domains;
 
     mapping(string => string) public records;
 
-    constructor() {
-        console.log("Init domains contract");
+    constructor(string memory _tld) {
+        tld = _tld;
+        console.log("Top Level Domain: %s", _tld);
     }
 
-    function register(string calldata name) public {
+    function price (string calldata name) public pure returns (uint) {
+        uint len = StringUtils.strlen(name);
+        require (len > 0, "String length zero");
+        if (len == 3) {
+            return 5 * 10**16; //0.05 MATIC since 10**18 is 1 MATIC
+        } else if (len == 4){
+            return  3 * 10**16;
+        } else {
+            return 1 * 10**16;
+        }
+    }
+
+    //And this my friends is a payable function. This is the power of blockchain where we added payments without
+    //any fancy API or auth. Smoooooth!!! When we call this fn through our run.js, we simpy put in the amount
+    //as the second argument
+    function register(string calldata name) public payable {
+
+        //We only mention storage location for array, struct or mapping type which are reference type variables.
+        //uint is value type, ie, value of these varibales are copied ev
+        uint _price = price(name);
+
+        //Require to have enough funds to cover the price
+        require(msg.value > _price, "Funds not enough.");
+
+
         //Here we’re checking that the address of the domain you’re trying to register
         //is the same as the zero address. The zero address in Solidity is sort of like the void (in the literal
         //sense) where everything comes from. When an address mapping is initialized, all entries in it point to the
--- scripts/run.js
+++ scripts/run.js
@@ -10,21 +10,26 @@ const main = async () => {
     const domainContractFactory = await hre.ethers.getContractFactory('Domains');
 
     //Deploys it to the chain configured in hardhat.config.js
-    const domainContract = await domainContractFactory.deploy();
+    const domainContract = await domainContractFactory.deploy("wagmi");
 
     //Wait till the contract is mined and deployed to the local blockchain especially created for this account
     //which will be destroyed once this script completes
     await domainContract.deployed();
     console.log("Contract deployed to: " + domainContract.address);
     console.log("Contract deployed by:" + owner.address);
-    console.log("What the hell is account: " + randomAccount);
+    console.log("What the hell is account: " + randomAccount.address);
 
-    let txn = await domainContract.register("0xshinigami");
+    //Second variable is how we pass money to the payable contract from
+    let txn = await domainContract.register("0xshinigami", {value: hre.ethers.utils.parseEther('0.1')});
     await txn.wait();
 
     const domainAddress = await domainContract.getAddress("0xshinigami");
     console.log("Address for domain name 0xshinigami: " + domainAddress);
-    
+
+    //Lets check the smart contract balance
+    const balance = await hre.ethers.provider.getBalance(domainContract.address);
+    console.log("Contract bal: " + hre.ethers.utils.formatEther(balance));
+
     // txn = await domainContract.connect(randomAccount).setRecord("0xshinigami", "Setting domain w/o being the
     // sender, should revert.");
     // await txn.wait();
